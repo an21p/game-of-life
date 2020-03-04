ac78f215e6c3962a0a2d6b9bf5d2e2586887fd56
Antonis Pishias <espen.folke@gmail.com>
2020-03-04 22:11:45 +0000

    remove cljs-out

--- resources/public/cljs-out/dev/cljs/user/game8AB992D.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 1.10.520 {}
-
-//# sourceMappingURL=game8AB992D.js.map
--- resources/public/cljs-out/dev/demo/core_test.cljs
+++
@@ -1,5 +0,0 @@
-(ns demo.core-test
-  (:require
-   [cljs.test :refer-macros [deftest is testing]]
-   ;;[demo.core :refer [multiply]]
-   ))
